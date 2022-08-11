00c4885787bb1ae8dc125088faadb1ac90ce8f2f
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-08-11 10:51:40 +0200

    LocalStorage works

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,7 @@ const addButton = document.querySelector("#newtask");
 const formButton = document.getElementById("addnew");
 
 let taskArr = [];
-
-// function newtask() {
-//   let addTask = {
-//     name: "",
-//     description: "",
-//     date: "",
-//   };
-//   let task = addTask;
-//   task.name = prompt("Enter a name for your task");
-//   task.description = prompt("Describe your task");
-//   task.date = new Date(prompt("Enter a date"));
-
-//   let datestart = new Date();
-//   let difftemps = task.date.getTime() - datestart.getTime();
-//   let difftempsjour = Math.ceil(difftemps / (1000 * 3600 * 24));
-
-//   taskArr.push(addTask);
-
-//   let cardTask = `
-//         <div class="main__card">
-//           <h2 class="card__title">${task.name}</h2>
-//           <p class="card__description">${task.description}</p>
-//           <p class="card__date">${difftempsjour + " jours restant"}</p>
-//           <img src="assets/img/flag.png" alt="" class="card__flag" />
-//         </div>`;
-//   document
-//     .querySelector(".main__todo")
-//     .insertAdjacentHTML("beforeend", cardTask);
-// }
+const getLocalArray = JSON.parse(localStorage.getItem("addTask") || "[]");
 
 function openModal() {
   const modal = document.getElementById("addTask");
@@ -40,19 +12,20 @@ function openModal() {
 addButton.addEventListener("click", openModal);
 
 function newtask() {
+  //Object
   let addTask = {
     name: "",
     description: "",
     date: "",
   };
+  //Create object
   let task = addTask;
   task.name = document.getElementById("name").value;
   task.description = document.getElementById("description").value;
   task.date = document.getElementById("date").value;
-
+  //Push object
   taskArr.push(addTask);
-  console.log(taskArr);
-
+  //Generate card
   let cardTask = `
             <div class="main__card">
               <h2 class="card__title">${task.name}</h2>
@@ -60,12 +33,35 @@ function newtask() {
               <p class="card__date">${task.date}</p>
               <img src="assets/img/flag.png" alt="" class="card__flag" />
             </div>`;
+  //Push card in column
   document
     .querySelector(".main__todo")
     .insertAdjacentHTML("beforeend", cardTask);
-
+  //Hide modal window
   const modal = document.getElementById("addTask");
   modal.style.visibility = "hidden";
+  //Add to local storage
+  if (localStorage.getItem("addTask") == null) {
+    localStorage.setItem("addTask", []);
+  }
+  getLocalArray.push(addTask);
+  localStorage.setItem("addTask", JSON.stringify(getLocalArray));
 }
+
+window.addEventListener("load", (e) => {
+  const add = getLocalArray.map((x) => {
+    let cardTask = `
+    <div class="main__card">
+      <h2 class="card__title">${x.name}</h2>
+      <p class="card__description">${x.description}</p>
+      <p class="card__date">${x.date}</p>
+      <img src="assets/img/flag.png" alt="" class="card__flag" />
+    </div>`;
+    //Push card in column
+    document
+      .querySelector(".main__todo")
+      .insertAdjacentHTML("beforeend", cardTask);
+  });
+});
 
 formButton.addEventListener("click", newtask);
